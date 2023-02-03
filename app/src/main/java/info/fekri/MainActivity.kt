938ecd618ab72909b79d1980094a0c7da0a628c1
fekri86114 <fekri.mohammadreza86@gmail.com>
2023-02-03 09:18:31 -0800

    second example | broadcast-receiver with permission

--- app/src/main/java/info/fekri/MainActivity.kt
+++ app/src/main/java/info/fekri/MainActivity.kt
@@ -1,40 +1,93 @@
 package info.fekri
 
+import android.Manifest.permission.RECEIVE_SMS
 import android.content.IntentFilter
+import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import info.fekri.databinding.ActivityMainBinding
-
-// I wanna receive connectivity-change of internet here.
-// It's better to receive data in the Activity.
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var myReceiver: MyBroadcastReceiver
+    private val SMS_REQUEST_CODE = 100
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.btnRequest.setOnClickListener {
+
+            if (
+                ContextCompat
+                    .checkSelfPermission(this, RECEIVE_SMS) != PackageManager.PERMISSION_GRANTED
+            ) {
+
+                requestSMSPermission()
+
+            } else {
+                Toast.makeText(this, "Permission has been Received!", Toast.LENGTH_SHORT).show()
+            }
+
+        }
+
     }
 
-    // It's better to do the receiving-work in the onResume and onPause methods -->
+    private fun requestSMSPermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, RECEIVE_SMS)) {
+
+            AlertDialog.Builder(this)
+                .setTitle("Request Permission")
+                .setMessage("Confirm the Permission to check SMS.")
+                .setPositiveButton("GO") { dialog, which -> reqPermission() }
+                .setNegativeButton("NO") { dialog, which -> dialog.dismiss() }
+                .create()
+                .show()
+
+        } else {
+            reqPermission()
+        }
+    }
+
+    private fun reqPermission() {
+        ActivityCompat.requestPermissions(this, arrayOf(RECEIVE_SMS), SMS_REQUEST_CODE)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if (requestCode == SMS_REQUEST_CODE) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "Permission Confirmed!", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Permission Denied!", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+    }
+
     override fun onResume() {
-        // I want to check version and register first,
-        // then parent things
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
             myReceiver = MyBroadcastReceiver()
-            registerReceiver(myReceiver, IntentFilter("android.net.conn.CONNECTIVITY_CHANGE"))
+            registerReceiver(myReceiver, IntentFilter("android.conn.CONNECTIVITY_CHANGE"))
         }
 
         super.onResume()
     }
 
-    // unregister myReceiver when application paused -->
     override fun onPause() {
-        super.onPause()
         unregisterReceiver(myReceiver)
+        super.onPause()
     }
 
 }
--- app/src/main/java/info/fekri/MyBroadcastReceiver.kt
+++ app/src/main/java/info/fekri/MyBroadcastReceiver.kt
@@ -1,34 +1,34 @@
 package info.fekri
 
+import android.bluetooth.BluetoothDevice
+import android.bluetooth.BluetoothManager
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.net.ConnectivityManager
 import android.widget.Toast
-
-
+import androidx.core.content.getSystemService
 
 class MyBroadcastReceiver : BroadcastReceiver() {
 
-    override fun onReceive(context: Context?, intent: Intent?) {
-
-        if (networkChecker(context!!)) {
-            Toast.makeText(context, "Internet is Connected!", Toast.LENGTH_LONG).show()
+    override fun onReceive(context: Context, intent: Intent) {
+        // check network connection -->
+        if (networkChecker(context)) {
+            Toast.makeText(context, "Internet is Connected!", Toast.LENGTH_SHORT).show()
         } else {
             Toast.makeText(context, "Internet is not Connected!", Toast.LENGTH_SHORT).show()
         }
 
+        Toast.makeText(context, "Received a Message!", Toast.LENGTH_SHORT).show()
     }
 
     private fun networkChecker(mContext: Context): Boolean {
-
         try {
             val conManager =
                 mContext.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
             val nInfo = conManager.activeNetworkInfo
 
             return (nInfo != null && nInfo.isConnected)
-
         } catch (e: Exception) {
             e.printStackTrace()
             return false
