47086543222622dbee5a9ab318bdcce5acb81936
fekri86114 <fekri.mohammadreza86@gmail.com>
2023-02-03 01:03:25 -0800

    second example | wifi checker

--- app/src/main/java/info/fekri/MainActivity.kt
+++ app/src/main/java/info/fekri/MainActivity.kt
@@ -4,44 +4,79 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
+import android.net.wifi.WifiManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Switch
+import android.widget.Toast
+import androidx.appcompat.widget.SwitchCompat
+import androidx.core.content.ContextCompat
 import info.fekri.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private lateinit var broadcastReceiver: BroadcastReceiver
+    private lateinit var wifiSwitch: SwitchCompat
+    private lateinit var wifiManager: WifiManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        broadcastReceiver = object : BroadcastReceiver() {
-            override fun onReceive(context: Context?, intent: Intent?) {
-                checkNetworkConnection()
+        wifiSwitch = binding.switchTurnOnOffWifi
+        wifiManager = getSystemService(WIFI_SERVICE) as WifiManager
+
+        wifiSwitch.setOnCheckedChangeListener { buttonView, isChecked ->
+            if (isChecked) {
+                wifiManager.isWifiEnabled = true
+                wifiSwitch.text = "Wifi is ON"
+            } else {
+                wifiManager.isWifiEnabled = false
+                wifiSwitch.text = "Wifi is OFF"
             }
         }
 
-        val intentFilter = IntentFilter("android.conn.CONNECTIVITY_CHANGE")
-        registerReceiver(
-            broadcastReceiver,
-            intentFilter
-        ) // register 'broadcastReceiver' with 'CONNECTIVITY intent'
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        val intentFilter = IntentFilter(WifiManager.WIFI_STATE_CHANGED_ACTION)
+        registerReceiver(wifiStateReceiver, intentFilter)
 
     }
 
-    private fun checkNetworkConnection() {
+    override fun onStop() {
+        super.onStop()
 
-        if (NetworkChecker(this).isInternetConnected)
-            binding.txtShowNetwork.text = "Internet Connected"
-        else
-            binding.txtShowNetwork.text = "Internet Disconnected"
+        unregisterReceiver(wifiStateReceiver)
 
     }
-    // unregister the broadcastReceiver
-    override fun onDestroy() {
-        super.onDestroy()
-        unregisterReceiver(broadcastReceiver)
+
+    private val wifiStateReceiver: BroadcastReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent?) {
+
+            when (intent!!.getIntExtra(
+                WifiManager.EXTRA_WIFI_STATE,
+                WifiManager.WIFI_STATE_UNKNOWN
+            )) {
+                WifiManager.WIFI_STATE_ENABLED -> {
+
+                    wifiSwitch.isChecked= true
+                    wifiSwitch.text = "Wifi is ON"
+                    Toast.makeText(this@MainActivity, "Wifi is ON", Toast.LENGTH_SHORT).show()
+
+                }
+                WifiManager.WIFI_STATE_DISABLED -> {
+
+                    wifiSwitch.text = "Wifi is OFF"
+                    wifiSwitch.isChecked = false
+                    Toast.makeText(this@MainActivity, "Wifi is OFF", Toast.LENGTH_SHORT).show()
+
+                }
+            }
+
+        }
     }
+
 }
